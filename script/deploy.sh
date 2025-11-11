5d8464032ff3756b0c6ae1d012017793f91a5900
centonhuang <centonhuang@tencent.com>
2025-11-11 12:32:04 +0800

    🐳 chore: update deploy script to pull aris-mem-api image and display its logs

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,5 @@
 echo -e "\033[1;32mPulling the latest Docker image...\033[0m"
-docker pull ghcr.io/hcd233/aris-blog-api:master
+docker pull ghcr.io/hcd233/aris-mem-api:master
 
 echo -e "\033[1;34mStarting up services with docker-compose...\033[0m"
 docker compose -f docker/docker-compose-without-middlewares.yml up -d
@@ -7,5 +7,5 @@ docker compose -f docker/docker-compose-without-middlewares.yml up -d
 echo -e "\033[1;31mPruning unused Docker images...\033[0m"
 docker image prune -a -f
 
-echo -e "\033[1;33mDisplaying Docker logs for aris-blog-api...\033[0m"
-docker logs -f aris-blog-api --details
+echo -e "\033[1;33mDisplaying Docker logs for aris-mem-api...\033[0m"
+docker logs -f aris-mem-api --details
