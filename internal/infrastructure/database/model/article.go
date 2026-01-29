e7d4c1cc621caa6c3147a4ec52e3730b79fbedaf
centonhuang <centonhuang@tencent.com>
2026-01-29 20:49:52 +0800

    🔧 refactor: update slug field in article and tag models to set default value for improved data integrity

--- internal/infrastructure/database/model/article.go
+++ internal/infrastructure/database/model/article.go
@@ -18,7 +18,7 @@ type Article struct {
 	ID          uint               `json:"id" gorm:"column:id;primary_key;auto_increment;comment:用户ID"`
 	UserID      uint               `json:"user_id" gorm:"column:user_id;not null;comment:用户ID"`
 	Title       string             `json:"title" gorm:"column:title;not null;comment:标题"`
-	Slug        string             `json:"slug" gorm:"column:slug;not null;uniqueIndex:idx_slug_deleted_at;comment:简写"`
+	Slug        string             `json:"slug" gorm:"column:slug;not null;default:'';uniqueIndex:idx_slug_deleted_at;comment:简写"`
 	Content     string             `json:"content" gorm:"column:content;not null;comment:内容"`
 	CoverImage  string             `json:"cover_image" gorm:"column:cover_image;comment:封面图片URL"`
 	PublishedAt time.Time          `json:"published_at" gorm:"column:published_at;not null;comment:发布时间"`
--- internal/infrastructure/database/model/tag.go
+++ internal/infrastructure/database/model/tag.go
@@ -9,5 +9,5 @@ type Tag struct {
 	DeletedAt int64  `json:"deleted_at" gorm:"column:deleted_at;uniqueIndex:idx_name_deleted_at;comment:删除时间，默认为0"`
 	ID        uint   `json:"id" gorm:"column:id;primary_key;auto_increment;comment:ID"`
 	Name      string `json:"name" gorm:"column:name;not null;uniqueIndex:idx_name_deleted_at;comment:名称"`
-	Slug      string `json:"slug" gorm:"column:slug;not null;comment:简写"`
+	Slug      string `json:"slug" gorm:"column:slug;not null;default:'';comment:简写"`
 }
