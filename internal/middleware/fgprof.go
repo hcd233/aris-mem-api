f99efac5475c3c252ea923108a9a0ff0bbe501f9
centonhuang <centonhuang@tencent.com>
2026-02-10 17:57:51 +0800

    🦄 refactor: tiny refactor

--- internal/middleware/fgprof.go
+++ internal/middleware/fgprof.go
@@ -11,5 +11,6 @@ import (
 //	@author centonhuang
 //	@update 2025-09-25 21:17:02
 func FgprofMiddleware() fiber.Handler {
+	// go tool pprof -http=:8081 http://xxxx/debug/fgprof
 	return fgprof.New()
 }
